9cf986cf6e2a90a6245af6e4f616b4109098db20
johhnymagome24@gmail.com <johhnymagome24@gmail.com>
2023-01-13 15:26:05 +0200

    changes to the flask app

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 import numpy as np
 import pandas as pd
 
@@ -12,6 +13,7 @@ df = df[(df.z != 0) & (df.x != 0) & (df.y != 0)]
 df1 = df[df.y < 30 ]
 df1 = df1[df1.z < 30 ]
 df1 = df1.drop(['Unnamed: 0','color','cut','clarity'],axis=1)
+df1 = np.log(df1)
 
 model = LinearRegression()
 X = df1.drop(['price','y','z'], axis=1)
@@ -27,13 +29,17 @@ def index():
 def predict():
     if request.method == 'POST':
         # Get the user input from the form
-        input_data = [float(request.form['carat']), float(request.form['x']), float(request.form['y']), float(request.form['z'])]
-
+        input_data = [float(request.form['carat']), float(request.form['depth']), float(request.form['table']), float(request.form['x'])]
+        
         # Use the model to make a prediction
         prediction = model.predict([input_data])
+        prediction = np.exp(prediction)
+        r2 = r2_score(y_train, model.predict(X_train))
+        skew = df1['price'].skew()
+        kurt = df1['price'].kurt()
 
         # Return the prediction to the user
-        return render_template('results.html', prediction=prediction[0])
+        return render_template('results.html', prediction=prediction[0],y_train=y_train,r2 = r2,skew = skew, kurt = kurt)
 
 if __name__ == '__main__':
     app.run(debug=True)

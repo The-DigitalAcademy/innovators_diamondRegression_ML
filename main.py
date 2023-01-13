c15608960aa0ba854ad487bba78be3d4a2f69e15
johhnymagome24@gmail.com <johhnymagome24@gmail.com>
2023-01-13 21:09:12 +0200

    added model pipeline , changes to dashboard

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, mean_squared_error
 import numpy as np
+import joblib as jb
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
 app = Flask(__name__)
+filename = "Diamond_model.joblib"
+model = jb.load(filename)
+
 
 # Load the Linear Regression mode
 df = pd.read_csv('diamonds.csv')
@@ -15,11 +18,9 @@ df1 = df1[df1.z < 30 ]
 df1 = df1.drop(['Unnamed: 0','color','cut','clarity'],axis=1)
 df1 = np.log(df1)
 
-model = LinearRegression()
 X = df1.drop(['price','y','z'], axis=1)
 y = df1['price']
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=49)
-model.fit(X_train,y_train)
 
 @app.route('/')
 def index():
@@ -34,14 +35,24 @@ def predict():
         # Use the model to make a prediction
         prediction = model.predict([input_data])
         prediction = np.exp(prediction)
-        
-        
+        prediction = np.around(prediction,2)
+
+        mse_train = mean_squared_error(y_train, model.predict(X_train))
+        mse_train = np.exp(mse_train)
+        mse_train = np.around(mse_train,2)
+        mse_test = mean_squared_error(y_test, model.predict(X_test))
+        mse_test = np.exp(mse_test)
+        mse_test = np.around(mse_test,2)
+
+        actual = prediction in np.asarray(df1)
+    
         r2 = r2_score(y_train, model.predict(X_train))
-        skew = df1['price'].skew()
-        kurt = df1['price'].kurt()
+        r2 = float(r2)
+        r2 = r2*100
+        r2 = round(r2,2)
 
         # Return the prediction to the user
-        return render_template('results.html', prediction=prediction[0],r2 = r2,skew = skew, kurt = kurt)
+        return render_template('results.html', prediction=prediction[0],actual=actual,r2 = r2,mse_train=mse_train,mse_test=mse_test)
 
 if __name__ == '__main__':
     app.run(debug=True)

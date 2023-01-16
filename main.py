489f9cd662e0df1139db4597685bb68edb5ff787
johhnymagome24@gmail.com <johhnymagome24@gmail.com>
2023-01-16 11:31:21 +0200

    fixes to code

--- main.py
+++ main.py
@@ -10,18 +10,6 @@ filename = "Diamond_model.joblib"
 model = jb.load(filename)
 
 
-# Load the Linear Regression mode
-df = pd.read_csv('diamonds.csv')
-df = df[(df.z != 0) & (df.x != 0) & (df.y != 0)]
-df1 = df[df.y < 30 ]
-df1 = df1[df1.z < 30 ]
-df1 = df1.drop(['Unnamed: 0','color','cut','clarity'],axis=1)
-df1 = np.log(df1)
-
-X = df1.drop(['price','y','z'], axis=1)
-y = df1['price']
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=49)
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -37,22 +25,9 @@ def predict():
         prediction = np.exp(prediction)
         prediction = np.around(prediction,2)
 
-        mse_train = mean_squared_error(y_train, model.predict(X_train))
-        mse_train = np.exp(mse_train)
-        mse_train = np.around(mse_train,2)
-        mse_test = mean_squared_error(y_test, model.predict(X_test))
-        mse_test = np.exp(mse_test)
-        mse_test = np.around(mse_test,2)
-
-        actual = prediction in np.asarray(df1)
-    
-        r2 = r2_score(y_train, model.predict(X_train))
-        r2 = float(r2)
-        r2 = r2*100
-        r2 = round(r2,2)
 
         # Return the prediction to the user
-        return render_template('results.html', prediction=prediction[0],actual=actual,r2 = r2,mse_train=mse_train,mse_test=mse_test)
+        return render_template('results.html', prediction=prediction[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
